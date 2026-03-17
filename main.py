590d67ecddacf648944a5fb1c797fb9fae87019c
pyadminru <zh.vitaliy92@yandex.ru>
2026-03-17 16:00:34 +0300

    Рефакторинг кода

--- main.py
+++ main.py
@@ -5,19 +5,11 @@ from loguru import logger
 from src.change_name_description_photo import handle_change_name_description_photo
 from src.commentator import TelegramCommentator
 from src.config_handler import (
-    program_version,
-    program_last_modified_date,
-    program_name,
-    app_log,
-    errors_log,
-    WINDOW_WIDTH,
-    WINDOW_HEIGHT,
+    program_version, program_last_modified_date, program_name, app_log, errors_log, WINDOW_WIDTH, WINDOW_HEIGHT,
 )
 from src.core.handle_connect_accounts import handle_connect_accounts
 from src.core.handlers import (
-    handle_documentation,
-    handle_creating_list_of_channels,
-    handle_settings,
+    handle_documentation, handle_creating_list_of_channels, handle_settings,
 )
 from src.core.views import PRIMARY_COLOR, TITLE_FONT_WEIGHT
 from src.getting_list_channels import handle_getting_list_channels
